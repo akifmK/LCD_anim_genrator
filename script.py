c01555e5e2fa9cb1035860a2f64ae68ba858f56e
Akif Koc <114242303+akifmK@users.noreply.github.com>
2025-05-29 21:06:46 +0300

    Update script.py
    -Modified for I2C Protocol.
    -New Future = Use GIF instead of frame PNGs

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
 import os
+import re
 from PIL import Image
 from wand.image import Image as wandImg
 
-wd = os.path.realpath(__file__)[0:len(os.path.realpath(__file__))-9]
-b_frames = os.path.join(wd,"0-big_frames")
-s_frames = os.path.join(wd,"1-small_frames")
-s_text = os.path.join(wd,"2-small_text")
-out_loc = os.path.join(wd, "3-output_file")
-out_file = os.path.join(os.path.join(out_loc, "output"),"output.ino")
 
+#Create local files
+wd = os.path.realpath(__file__)[0:len(os.path.realpath(__file__))-9]
+gif = os.path.join(wd, "0-gif")
+b_frames = os.path.join(wd,"1-big_frames")
+s_frames = os.path.join(wd,"2-small_frames")
+s_text = os.path.join(wd,"3-small_text")
+out_loc = os.path.join(wd, "4-output_file")
+
+if not os.path.exists(gif):
+    os.makedirs(gif)
 if not os.path.exists(b_frames):
     os.makedirs(b_frames)
 if not os.path.exists(s_frames):
@@ -17,15 +22,29 @@ if not os.path.exists(s_text):
     os.makedirs(s_text)
 if not os.path.exists(out_loc):
     os.makedirs(out_loc)
-if not os.path.exists(os.path.join(out_loc,"output")):
-    os.makedirs(os.path.join(out_loc,"output"))
+# ==========
 
-print("Please place frames in "+b_frames+" and press enter.\n")
+# Get the name of the gif and create output file
+print("Please place your gif in "+gif+" and press enter.\n")
 input("[Press ENTER when you're ready.]\n")
-print(str(len(os.listdir(b_frames)))+" files found.\n")
-print("The folders 1 to 3 will be cleared. be sure that there's nothing you need still in there.\n")
+print(str(len(os.listdir(gif)))+" files found.\n")
+
+for a in os.listdir(gif):
+    outname = a[0:len(a)-4] + "_lcd_animated"  
+out_file = os.path.join(os.path.join(out_loc, outname), outname + ".ino")
+
+if not os.path.exists(os.path.join(out_loc,outname)):
+    os.makedirs(os.path.join(out_loc,outname))
+
+print("The folders 0 to 3 will be cleared. be sure that there's nothing you need still in there.\n")
 input("[Press ENTER to start.]\n")
+# ==========
+
 # Clearing folders
+print("Clearing "+b_frames)
+for f in os.listdir(b_frames):
+    os.remove(os.path.join(b_frames,f))
+print("Done.")
 print("Clearing "+s_frames)
 for f in os.listdir(s_frames):
     os.remove(os.path.join(s_frames,f))
@@ -39,6 +58,28 @@ if os.path.isfile(out_file):
     os.remove(out_file)
     print("Done.\n")
 # ========== 
+
+# gif to png frames
+print("Changing gif to png and saving to "+b_frames)
+
+for filename in os.listdir(gif):
+    if filename.lower().endswith('.gif'):
+        gif_path = os.path.join(gif, filename)
+
+frame = Image.open(gif_path)
+nframes = 0
+while frame:
+        frame.save( '%s/%s_%s.png' % (b_frames, os.path.splitext(os.path.basename(gif_path))[0], nframes ) )
+        nframes += 1
+        try:
+            frame.seek( nframes )
+        except EOFError:
+            break;
+
+print("DONE converting gif to png frames."+b_frames)
+# ==========
+
+
 print("Changing size and saving to "+s_frames)
 # conversion
 for f in sorted(os.listdir(b_frames)):
@@ -47,6 +88,8 @@ for f in sorted(os.listdir(b_frames)):
         img.threshold(0.5)
         img.save(filename = os.path.join(s_frames,f))
 # ==========
+
+
 print("DONE converting. (1/2)\n")
 print("Converting to byte arrays and saving in "+s_text)
 # Conversion
@@ -91,12 +134,25 @@ for frame in sorted(os.listdir(s_frames)):
             f.close()
 # ==========
 print("DONE converting. (2/2)\n")
+
+# lcd address input
+while True:
+    lcd_address = input("Give your LCD Screen Addrees (for example 0x27) : \n")
+
+    if re.fullmatch(r'[0-9]x[0-9]{2}', lcd_address):
+        print("Valid address:", lcd_address)
+        break  
+    else:
+        print("Wrong format!!  True format example(0x19 =>  number x number number\n)")
+# ==========      
+
+
 print("Building .ino file in "+out_loc)
 # Building
 f = open(out_file,"w")
-f.write("#include <LiquidCrystal.h>\n")
-f.write("const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5;\n")
-f.write("LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);\n")
+f.write("#include <LiquidCrystal_I2C.h>\n")
+f.write("#include <Wire.h>\n")
+f.write("LiquidCrystal_I2C lcd("+ lcd_address +",16,2);\n")
 # b arrays
 for bfile in sorted(os.listdir(s_text)):
     bpos = 0
@@ -114,7 +170,10 @@ for bfile in sorted(os.listdir(s_text)):
             print("LINES TOO SHORT OR LONG IN "+os.path.join(s_text,bfile)+" AT LINE "+str(bpos)+" LENGTH "+str(len(bline)-1)+" INSTEAD OF 5")
     f.write("};\n")
 # setup
-f.write("void setup() { lcd.begin(16,2); }\n")
+f.write("void setup() { \n")
+f.write("lcd.init();\n")
+f.write("lcd.backlight();\n")
+f.write("lcd.clear(); }\n")
 # loop
 f.write("void loop() {\n")
 # writes
@@ -150,6 +209,10 @@ print("DONE building file. The file you're looking for is "+out_file)
 print("\nThe temporary files can be cleaned up automatically. If you want that, press now enter. (Doesn't afffect original frames.)")
 input("[Press ENTER for cleanup.]\n")
 # Cleanup
+print("Clearing "+b_frames)
+for f in os.listdir(b_frames):
+    os.remove(os.path.join(b_frames,f))
+print("Done.")
 print("Clearing "+s_frames)
 for f in os.listdir(s_frames):
     os.remove(os.path.join(s_frames,f))
